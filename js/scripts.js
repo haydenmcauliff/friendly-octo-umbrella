432d3f23f282cbc90e7676ab4051aaaedb5d8e9a
Hayden McAuliff <129900638+haydenmcauliff@users.noreply.github.com>
2023-06-08 00:32:43 +0000

    copied from codepen

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,52 @@
 console.log('OK');
+gsap.to( '#vine1', {
+    y: -80,
+    opacity: 100,
+    duration: 5,
+    repeat: -1,
+    yoyo: true
+  });
+  gsap.to( '#vine5', {
+    y: -30,
+    opacity: 100,
+    duration: 4,
+    repeat: -1,
+    yoyo: true,
+    delay: 4
+  });
+  gsap.to( '#vine6', {
+    y: -50,
+    opacity: 100,
+    duration: 5,
+    repeat: -1,
+    yoyo: true,
+    delay: 2,
+    scale: 1.5
+  });
+  gsap.to( '#vine4', {
+    y: -10,
+    opacity: 100,
+    duration: 4,
+    repeat: -1,
+    yoyo: true,
+    delay: 1,
+    scale: 1.2
+  });
+  gsap.to( '#vine3', {
+    y: -50,
+    opacity: 100,
+    duration: 4,
+    repeat: -1,
+    yoyo: true,
+    delay: 3,
+    scale: 1.3
+  });
+  gsap.to( '#vine2', {
+    y: -20,
+    opacity: 100,
+    duration: 4,
+    repeat: -1,
+    yoyo: true,
+    delay: 5,
+    scale: 1.3
+  });
